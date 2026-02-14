57b800cdf3992ca58e12791f55d672b6b59c2626
Rafi Siddique <rafisiddique67@gmail.com>
2026-02-14 13:42:17 +0600

    handle not found

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import routes from "./app/routes/index.routes";
+import httpStatus from "http-status";
 
 const app: Application = express();
 
@@ -10,5 +11,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", routes);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Not Found",
+    errorMessage: [
+      {
+        path: req.originalUrl,
+        message: "API Not Found",
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
