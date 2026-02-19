183361d6cc4fe63fec0981cb39621d34a39c9289
Rafi Siddique <rafisiddique67@gmail.com>
2026-02-19 13:22:32 +0600

    [Done] add user validation

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { UserController } from "./user.controller";
+import { UserValidation } from "./user.validation";
+import validateRequest from "../../middlewares/validateRequest";
 
 const router = express.Router();
 
-router.post("/auth/signup", UserController.createUser);
+router.post(
+  "/auth/signup",
+  validateRequest(UserValidation.createUserZodSchema),
+  UserController.createUser,
+);
 router.get("/:id", UserController.getSingleUser);
 router.get("/", UserController.getAllUsers);
 router.patch("/:id", UserController.updateUser);
