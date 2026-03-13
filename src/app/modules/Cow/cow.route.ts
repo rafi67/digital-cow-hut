f2fcb38438f6f6ecbb0e44873aa304d51813637b
Rafi Siddique <rafisiddique67@gmail.com>
2026-03-13 19:18:36 +0600

    [Done] add auth guard in cow module

--- src/app/modules/Cow/cow.route.ts
+++ src/app/modules/Cow/cow.route.ts
@@ -2,22 +2,34 @@ import express from "express";
 import { CowController } from "./cow.controller";
 import validateRequest from "../../middlewares/validateRequest";
 import { CowValidation } from "./cow.validation";
+import auth from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enums/user";
 
 const router = express.Router();
 
 router.post(
   "/",
   validateRequest(CowValidation.createCowZodSchema),
+  auth(ENUM_USER_ROLE.SELLER),
   CowController.createCow,
 );
 
-router.get("/:id", CowController.getSingleCow);
-router.get("/", CowController.getAllCows);
+router.get(
+  "/:id",
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SELLER, ENUM_USER_ROLE.BUYER),
+  CowController.getSingleCow,
+);
+router.get(
+  "/",
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.BUYER, ENUM_USER_ROLE.SELLER),
+  CowController.getAllCows,
+);
 router.patch(
   "/:id",
   validateRequest(CowValidation.updateCowZodSchema),
+  auth(ENUM_USER_ROLE.SELLER),
   CowController.updateCow,
 );
-router.delete("/:id", CowController.deleteCow);
+router.delete("/:id", auth(ENUM_USER_ROLE.SELLER), CowController.deleteCow);
 
 export const CowRoutes = router;
