f8bea71cf9491d0fb08e960b4af4272e361bb1d8
Rafi Siddique <rafisiddique67@gmail.com>
2026-03-13 19:21:38 +0600

    [Done] add auth guard in order

--- src/app/modules/Order/order.route.ts
+++ src/app/modules/Order/order.route.ts
@@ -2,15 +2,22 @@ import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { OrderValidation } from "./order.validation";
 import { OrderController } from "./order.controller";
+import auth from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enums/user";
 
 const router = express.Router();
 
 router.post(
   "/",
   validateRequest(OrderValidation.createOrderZodSchema),
+  auth(ENUM_USER_ROLE.BUYER),
   OrderController.createOrder,
 );
 
-router.get("/", OrderController.getAllOrder);
+router.get(
+  "/",
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.BUYER, ENUM_USER_ROLE.SELLER),
+  OrderController.getAllOrder,
+);
 
 export const OrderRoutes = router;
